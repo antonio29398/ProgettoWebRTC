8fd68a4da343f29d3af324d05604f342dd78ecbb
antonio <antonionapolitano58@yahoo.it>
2024-01-08 14:04:33 +0000

    aggiunto mysql per collegamento a database

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
-var app = require('express')();
-var http = require('http').Server(app);
-const express = require('express');
+// var app = require('express')();
+// var http = require('http').Server(app);
+// const express = require('express');
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get('/',function(req, res){
-    res.sendFile(__dirname +'/index.html');
+// app.get('/',function(req, res){
+//     res.sendFile(__dirname +'/index.html');
 
+// });
+
+// http.listen(8080,function(){
+//     console.log('Listening on port 8080...');
+// });
+
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+  host: "172.18.0.4",
+  user: "root",
+  password: "piopio",
+ 
 });
 
-http.listen(8080,function(){
-    console.log('Listening on port 8080...');
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
