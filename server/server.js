08dc812dfd3204116134155dacac3fc769c3e0a6
antonio <antonionapolitano58@yahoo.it>
2024-01-10 18:04:53 +0000

    aggiunti certificati per https

--- server/server.js
+++ server/server.js
@@ -1,28 +1,51 @@
-// var app = require('express')();
-// var http = require('http').Server(app);
-// const express = require('express');
+var app = require('express')();
+// var https = require('https').Server(app);
+var https = require('https');
+const express = require('express');
+const  fs = require('fs')
+const port = 8080
 
-// app.use(express.static("public"));
+app.use(express.static("collegamenti"));
 
-// app.get('/',function(req, res){
-//     res.sendFile(__dirname +'/index.html');
+app.get('/',function(req, res){
+    res.sendFile(__dirname +'/index.html');
 
-// });
+});
 
-// http.listen(8080,function(){
+const httpsOptions = {
+    key:  fs.readFileSync('./security/localhost.key'),
+    cert: fs.readFileSync('./security/localhost.crt')
+}
+
+
+
+const server = https.createServer(httpsOptions, app).listen(port, () => {
+    console.log('server running at ' + port)
+  })
+
+// https.listen(8080,function(){
 //     console.log('Listening on port 8080...');
 // });
 
-var mysql = require('mysql2');
+// var mysql = require('mysql2');
 
-var con = mysql.createConnection({
-  host: "172.18.0.4",
-  user: "root",
-  password: "piopio",
+// var con = mysql.createConnection({
+//   host: "172.18.0.4",
+//   user: "root",
+//   password: "piopio",
  
-});
+// });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   con.end((err) => {
+//     if(err){
+//       console.error('Errore nella chiusura della connessione', err);
+//     }
+//     console.log('Connessione chiusa.');
+
+//   });
+// });
+
+
